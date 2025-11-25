8ed86c51f625b54909af30fe1eb9975331d8a555
ChinatsuY <plarail0324@gmail.com>
2025-11-25 12:19:11 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,36 @@
+<?php
+echo $score1 = 80;
+echo '<br>';
+
+echo $score2 = 60;
+echo '<br>';
+
+echo $score3 = 55;
+echo '<br>';
+
+echo $score4 = 40;
+echo '<br>';
+
+echo $score5 = 100;
+echo '<br>';
+
+echo $score6 = 25;
+echo '<br>';
+
+echo $score7 = 80;
+echo '<br>';
+
+echo $score8 = 95;
+echo '<br>';
+
+echo $score9 = 30;
+echo '<br>';
+
+echo $score10 = 60;
+echo '<br>';
+
+echo $all_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+echo '<br>';
+
+echo $all_score/10;
+?>
